438ff94732caab62edfc61ec52487d9c6e3e7d95
Sergo <sergokuzneczow@gmail.com>
2026-01-30 15:42:01 +0300

    Add navigator module.

---
+++ core/navigator/build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.google.dagger.hilt.android)
+    alias(libs.plugins.navigation.component.safeargs)
+}
+
+android {
+    namespace = "com.sergokuzneczow.navigator"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+kotlin {
+    explicitApi = ExplicitApiMode.Strict
+}
+
+dependencies {
+    //Kotlin
+    implementation(libs.kotlinx.serialization.json)
+    //Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    //Navigation component
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.navigation.dynamic.features)
+}
---
+++ core/navigator/src/main/kotlin/com/sergokuzneczow/navigator/NavigatorApi.kt
@@ -0,0 +1,7 @@
+package com.sergokuzneczow.navigator
+
+import androidx.navigation.NavController
+
+public interface NavigatorApi {
+    public fun toHome(navController: NavController)
+}
